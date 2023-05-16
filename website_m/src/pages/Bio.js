bc14a11f607f4e5070d25375bbc0625fcf830ccc
Malena Guallar <malena.guallar@gmail.com>
2023-05-17 00:53:14 +0200

    created architecture of app

---
+++ website_m/src/pages/Bio.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Bio = () => {
+
+    return (
+
+        <div>
+            <h1>about me</h1>
+            <p>D’abord organisatrice de fêtes libres au sein du collectif Astral, c’est justement là qu’elle a pu trouver la liberté de jouer ce qu’elle aimait. Ayant commencé à pratiquer en 2016 essentiellement avec de la techno, et parce qu’un style évolue sans cesse, aujourd’hui c’est techno toujours, break, acid, mais demain peut-être dnb ou reggaeton, qui sait. Dans tous les cas elle aime ne pas savoir ce qu’il va se passer à l’avance, pour ressentir le frisson de la vie sans doute. 
+                On ne compte plus les fêtes libres dans lesquelles elle a pu se produire. Pour ce qui est des évènements plus classiques, on a pu la retrouver à l’International, au 9B, et en warehouse. 
+Elle est résidente du collectif Ultracks.</p>
+        </div>
+    )
+
+}
+
+export default Bio ; 
