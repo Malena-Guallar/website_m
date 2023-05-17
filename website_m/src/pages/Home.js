17eb6cf5e41813f2b17e3c9f6397a74ef729022c
Malena Guallar <malena.guallar@gmail.com>
2023-05-17 10:47:40 +0200

    installed postcss, started styling

--- website_m/src/pages/Home.js
+++ website_m/src/pages/Home.js
@@ -5,10 +5,13 @@ const Home = () => {
 
     return (
 
+        <body className="h-screen bg-gradient-to-tr from-mint to-light_blue">
         <div>
             <Nav />
             <h1>welcome</h1>
         </div>
+
+        </body>
     )
 
 }
--- website_m/tailwind.config.js
+++ website_m/tailwind.config.js
@@ -1,7 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+  ],
   theme: {
+    colors: {
+      'green': '#aeff05',
+      'yellow': "#feff05",
+      'purple' : '#f390ff',
+      'mint': '#aeeebd',
+      'light_blue': '#94bbe9',
+      'white': '#ffffff'
+    },
     extend: {},
   },
   plugins: [],
